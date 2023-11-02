78c88f7d5e84cb2ff4eaba46ccffd5b8b176b380
Ferjuan NetLinux <fbatres@gmail.com>
2023-11-02 20:05:35 -0300

    proyecto deployado en front y back

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import NavBar from "./Components/NavBar/NavBar";
 import {Home,LandingPage,Detail,Form} from "./Views"
 import { Routes, Route, useLocation } from "react-router-dom";
 import axios from "axios";
-axios.defaults.baseURL = 'http://localhost:3001/'
+axios.defaults.baseURL = 'https://pokemondeployed-production.up.railway.app/'
 function App() {
   const location = useLocation();
 
